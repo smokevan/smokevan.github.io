eadfe6be271825cd43ba033d4d6694351eabcba5
Ivan Mokeyev <ivansmokeyev@gmail.com>
2025-10-02 10:23:43 -0400

    chore: restore mock images; constrain doc and project thumbnails; tweak docs layout

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -9,12 +9,12 @@ function Project() {
         <h1>some projects:</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="#/docs/c2s2"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="#/docs/c2s2"><img src={mock10} className="zoom" alt="thumbnail"/></a>
                 <a href="#/docs/c2s2"><h2>Cornell Custom Sillicon Systems</h2></a>
                 <p>Taping out chips as an undergraduate, working on writing RTL and physical design tooling scripts for efficient, customizable flows.</p>
             </div>
             <div className="project">
-                <a href="#/docs/TRV2_processor"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="#/docs/TRV2_processor"><img src={mock09} className="zoom" alt="thumbnail"/></a>
                 <a href="#/docs/TRV2_processor"><h2>TinyRiscV2 Processor</h2></a>
                 <p>Designed pipelined microprocessor with forwarding and in SVerilog from scratch, including 32-bit ALU with a variable cycle iterative algorithm for multiplication, streaming memory interface, and TinyRISCV2 instruction set compatibility. Working on multi-core.</p>
             </div>
